b52b6a61dc6bd7bc411e7cf842e08c6589943a41
aubymori <aubyomori@gmail.com>
2025-11-06 09:45:36 -0600

    Fix tilted arrow cutoff

--- assets/js/read_x2_combo.js
+++ assets/js/read_x2_combo.js
@@ -17,3 +17,17 @@ document.addEventListener("click", (event) =>
     if (!document.body.classList.contains("desktop"))
         document.getElementById("content").classList.toggle("text-2");
 });
+
+// Need to calculate height of tilted arrow to style it properly
+// Can't do that with CSS
+let tiltedArrow;
+if (tiltedArrow = document.querySelector(".tilted-arrow-link"))
+{
+    let tiltedArrowHeight = Math.round(tiltedArrow.getBoundingClientRect().height);
+    let arrowHeight = Math.round(tiltedArrow.parentElement.getBoundingClientRect().height);
+    let arrowPadding = tiltedArrowHeight - arrowHeight;
+
+    document.documentElement.style.setProperty(
+        "--arrow-padding",
+        arrowPadding + "px");
+}
